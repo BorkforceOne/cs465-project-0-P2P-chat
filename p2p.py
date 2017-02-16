3aa0a9effd5cf361db2e98f9ae747d7e043d415a
Brandon Garling <bjg96@nau.edu>
2017-02-16 00:12:06 -0700

    Fixup chat functionality to crash less

--- p2p.py
+++ p2p.py
@@ -6,12 +6,12 @@ from threading import Thread
 MSGLEN = 1024
 
 MSG_JOIN_NETWORK = 0X01
-MSG_ACKNOWLEDGE_JOIN = 0X01
 MSG_PEER = 0X02
 MSG_LEAVE = 0X03
 MSG_CHAT_MESSAGE = 0X04
 MSG_PEER_REQUEST = 0X05
 MSG_PEER_LIST = 0X06
+MSG_ACKNOWLEDGE_JOIN = 0X07
 
 MSG_HEADER_LEN = 4
 MSG_BUFFER_SIZE = 2048
@@ -33,58 +33,69 @@ def unpack_helper(fmt, data):
 
 class Peer(Thread):
 
-    def __init__(self, socket, name=None):
+    def __init__(self, socket, name="", host="", port=-1):
         Thread.__init__(self)
-        print("[INFO] Peer created")
+        print("[INFO] Peer connecting...")
 
         self.name = name
-        self.socket = socket
+        self.sock = socket
+        self.host = host
+        self.port = port
 
     def run(self):
         self.receive()
 
     def receive(self):
-        while True:
+        is_running = True
+        while is_running:
             data = ''
-            msg_len = safe_recv(self.socket, MSG_HEADER_LEN)
+            try:
+                msg_len = safe_recv(self.sock, MSG_HEADER_LEN)
+            except socket.error:
+                is_running = False
+                break
+
             msg_len = struct.unpack('<I', msg_len)[0]
 
             read_len = 0
             while read_len < msg_len:
-                chunk = safe_recv(self.socket, MSG_BUFFER_SIZE)
+                try:
+                    chunk = safe_recv(self.sock, MSG_BUFFER_SIZE)
+                except socket.error:
+                    is_running = False
+                    break
+
                 if not chunk:
                     break
                 data += chunk
                 read_len += len(chunk)
 
             while data != "":
-                ((message_id), data) = unpack_helper('<B', data)
-                message_id = message_id[0]
+                ((message_id,), data) = unpack_helper('<B', data)
+
+                if message_id == MSG_LEAVE:
+                    print("[CHAT] %s has left the chat", self.name)
+                    peers.remove(self)
+                    is_running = False
 
                 if message_id == MSG_CHAT_MESSAGE:
                     # Read all data for this message
-                    ((chat_len), data) = unpack_helper('<I', data)
-                    chat_len = chat_len[0]
-                    ((message), data) = unpack_helper('<%ds' % chat_len, data)
-                    message = message[0]
+                    ((chat_len,), data) = unpack_helper('<I', data)
+                    ((message,), data) = unpack_helper('<%ds' % chat_len, data)
                     print("[CHAT] %s says: %s" % (self.name, message))
 
                 elif message_id == MSG_JOIN_NETWORK:
-                    ((host_len), data) = unpack_helper('<I', data)
-                    host_len = host_len[0]
-                    ((host), data) = unpack_helper('<%ds' % host_len, data)
-                    self.host = host[0]
+                    ((host_len,), data) = unpack_helper('<I', data)
+                    ((self.host,), data) = unpack_helper('<%ds' % host_len, data)
+                    ((self.port,), data) = unpack_helper('<h', data)
+                    ((name_len,), data) = unpack_helper('<I', data)
+                    ((self.name,), data) = unpack_helper('<%ds' % name_len, data)
 
-                    ((port), data) = unpack_helper('<h', data)
-                    self.port = port[0]
+                    print("[INFO] %s has connected" % self.name)
 
-                    ((name_len), data) = unpack_helper('<I', data)
-                    name_len = name_len[0]
-                    ((name), data) = unpack_helper('<%ds' % name_len, data)
-                    self.name = name[0]
-
-                    #((guid), data) = unpack_helper('q', data)
-                    #guid = guid[0]
+                    packet = struct.pack("<B", MSG_ACKNOWLEDGE_JOIN)
+                    packet += client.serialize()
+                    self.send(packet)
 
                 elif message_id == MSG_PEER_REQUEST:
                     packet = struct.pack("<BI", MSG_PEER_LIST, len(peers) - 1)
@@ -94,45 +105,55 @@ class Peer(Thread):
                         packet += peer.serialize()
                     self.send(packet)
 
+                elif message_id == MSG_ACKNOWLEDGE_JOIN:
+                    ((host_len,), data) = unpack_helper('<I', data)
+                    ((self.host,), data) = unpack_helper('<%ds' % host_len, data)
+                    ((self.port,), data) = unpack_helper('<h', data)
+                    ((name_len,), data) = unpack_helper('<I', data)
+                    ((self.name,), data) = unpack_helper('<%ds' % name_len, data)
+
+                    print("[INFO] Connected to %s at %s:%d" % (self.name, self.host, self.port))
+
                 elif message_id == MSG_PEER_LIST:
-                    ((peers_len), data) = unpack_helper('<I', data)
-                    peers_len = peers_len[0]
+                    ((peers_len,), data) = unpack_helper('<I', data)
                     for i in range(peers_len):
                         # Connect to each peer identified
 
-                        ((host_len), data) = unpack_helper('<I', data)
-                        host_len = host_len[0]
-                        ((host), data) = unpack_helper('<%ds' % host_len, data)
-                        host = host[0]
-
-                        ((port), data) = unpack_helper('<h', data)
-                        port = port[0]
-
-                        ((name_len), data) = unpack_helper('<I', data)
-                        name_len = name_len[0]
-                        ((name), data) = unpack_helper('<%ds' % name_len, data)
-                        name = name[0]
+                        ((host_len,), data) = unpack_helper('<I', data)
+                        ((host,), data) = unpack_helper('<%ds' % host_len, data)
+                        ((port,), data) = unpack_helper('<h', data)
+                        ((name_len,), data) = unpack_helper('<I', data)
+                        ((name,), data) = unpack_helper('<%ds' % name_len, data)
 
                         peer_socket = socket.socket()
                         peer_socket.connect((host, port))
 
-                        peer = Peer(peer_socket, name)
+                        peer = Peer(peer_socket, name, host, port)
 
                         packet = struct.pack("<BI%dshI%ds" % (len(client.host), len(client.name)), MSG_JOIN_NETWORK,
                                              len(client.host), client.host, client.port, len(client.name), client.name)
                         peer.send(packet)
 
+                        print("[INFO] Connected to %s at %s:%d" % (name, host, port))
+
                         peers.append(peer)
                         peer.start()
 
                 if data != "":
-                    (nothing, data) = unpack_helper('<I', data)
+                    ((msg_len,), data) = unpack_helper('<I', data)
+
+        self.sock.close()
+
+    def send_leave(self):
+        packet = struct.pack("<B", MSG_LEAVE)
+        self.send(packet)
+        self.sock.close()
 
     def send(self, packed):
         packed = struct.pack('<I', len(packed)) + packed
         totalsent = 0
         while totalsent < len(packed):
-            sent = self.socket.send(packed[totalsent:])
+            sent = self.sock.send(packed[totalsent:])
             if sent == 0:
                 raise RuntimeError("socket connection broken")
             totalsent += sent
@@ -140,6 +161,8 @@ class Peer(Thread):
     def serialize(self):
         return struct.pack("<I%dshI%ds" % (len(self.host), len(self.name)), len(self.host), self.host, self.port, len(self.name), self.name)
 
+    def __str__(self):
+        return "(%s:%d, %s)" % (self.host, self.port, self.name)
 
 class Client:
 
@@ -191,19 +214,23 @@ class Client:
 
                 packet = struct.pack("<B", MSG_PEER_REQUEST)
                 peer.send(packet)
-                continue
 
             elif command[0] == 'say':
                 str = ' '.join(command[1:])
-                packed = struct.pack("<BI%ds" % len(str), MSG_CHAT_MESSAGE, len(str), str)
+                packet = struct.pack("<BI%ds" % len(str), MSG_CHAT_MESSAGE, len(str), str)
                 for peer in peers:
-                    peer.send(packed)
+                    peer.send(packet)
                 # Do chat message
-                continue
+                print("[CHAT] %s says: %s" % (self.name, str))
+
+            elif command[0] == 'peers':
+                for peer in peers:
+                    print(peer)
 
             elif command[0] == 'exit':
+                for peer in peers:
+                    peer.send_leave()
                 exit()
-                continue
 
     def listen(self):
         print("[INFO] Listening for new peers on port " + str(self.port) + "...")
@@ -220,6 +247,9 @@ class Client:
             peer = Peer(peer_socket)
             peers.append(peer)
             peer.start()
+
+    def serialize(self):
+        return struct.pack("<I%dshI%ds" % (len(self.host), len(self.name)), len(self.host), self.host, self.port, len(self.name), self.name)
 
 print("====Super Awesome P2P Chat For Super Awesome People (SAP2PCFSAP for short)====")
 
